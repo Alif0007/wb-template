ff353116a383bc699ce755e20aa8ba497f97e389
Alif0007 <alaminalif373@gmail.comm>
2024-07-06 15:56:37 +0600

    hero section completed

---
+++ script.js
@@ -0,0 +1,40 @@
+
+
+const ctx = document.getElementById('myChart');
+
+new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: ['Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul'],
+        datasets: [{
+            label: ' WPPOOL   ',
+            data: [12, 19, 51, 5, 2, 39],
+            borderWidth: 1
+        },
+        {
+            label: 'Google',
+            data: [12, 40, 3, 5, 32, 3],
+            borderWidth: 1
+        },
+        {
+            label: 'Microsoft',
+            data: [12, 40, 3, 73, 32, 3],
+            borderWidth: 1
+        },
+        {
+            label: 'Twitters',
+            data: [62, 40, 3, 5, 32, 3],
+            borderWidth: 1
+        }
+        ]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            },
+
+
+        }
+    }
+});
